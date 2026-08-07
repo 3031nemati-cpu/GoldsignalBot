8fa0e574d6df8d513f8342440866db4bbc2a5240
3031nemati-cpu <3031nemati@gmail.com>
2026-08-07 07:58:42 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
-API_KEY = os.getenv("TWELVEDATA_API_KEY")
+API_KEY = os.getenv("API_KEY")
 
 SYMBOL = "XAU/USD"
 
